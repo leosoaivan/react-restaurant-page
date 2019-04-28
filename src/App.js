f6380ce08b1ac0806de3213eea4ed9d8bdd05c68
LPSV <leo.soaivan@gmail.com>
2019-04-27 23:55:50 -0400

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Shrooms from './images/kingoystershrooms.jpeg';
+import Header from './components/Header';
 
 const Root = styled.div`
   height: 100%;
@@ -10,7 +11,9 @@ const Root = styled.div`
 
 const App = () => {
   return (
-    <Root />
+    <Root>
+      <Header />
+    </Root>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import Raleway from 'typeface-raleway';
+
+const Root = styled.header`
+  background-color: black;
+  color: white; 
+  display: flex;
+  justify-content: center;
+`;
+  
+  const HeaderText = styled.p`
+  font-family: Raleway, sans-serif;
+  font-size: 1.25em;
+  font-weight: 400;
+  line-height: 2.25em;
+  margin: 0 1.5em 0 1.5em;
+`;
+
+const Header = () => {
+  return (
+    <Root>
+      <HeaderText>
+        {'835 North Capital Street, Alexandria, VA 12345'}
+      </HeaderText>
+      <HeaderText>
+        {'(123) 456-7890'}
+      </HeaderText>
+
+    </Root>
+  )
+}
+
+export default Header;

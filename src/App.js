4b75ba7336d9b5c9d766ae1cb5f200a94856474b
LPSV <leo.soaivan@gmail.com>
2019-04-27 23:26:07 -0400

    Add initial background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Shrooms from './images/kingoystershrooms.jpeg';
 
-function App() {
+const Root = styled.div`
+  height: 100%;
+  width: 100%;
+  background-image: url(${Shrooms});
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root />
   );
 }
 
